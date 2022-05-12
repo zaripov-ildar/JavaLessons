9e87185da067fd7d874d574b485e9199735b5bf7
Ildar Zaripov <zariil@yandex.ru>
2022-05-12 22:12:32 +0500

    Homework 7

--- src/main/java/ru/gb/zaripov/homework6/Cat.java
+++ src/main/java/ru/gb/zaripov/homework6/Cat.java
@@ -1,10 +1,12 @@
 package ru.gb.zaripov.homework6;
 
 public class Cat extends Animal {
+    private static final int RUN_LIMIT = 200;
+    private static final int SWIM_LIMIT = 0;
     private static int catsAmount;
 
     public Cat(String name) {
-        super(200, 0, name);
+        super(RUN_LIMIT, SWIM_LIMIT, name);
         catsAmount++;
     }
 
--- src/main/java/ru/gb/zaripov/homework6/Dog.java
+++ src/main/java/ru/gb/zaripov/homework6/Dog.java
@@ -1,10 +1,12 @@
 package ru.gb.zaripov.homework6;
 
 public class Dog extends Animal {
+    private static final int RUN_LIMIT = 500;
+    private static final int SWIM_LIMIT = 10;
     private static int dogsAmount;
 
     public Dog(String name) {
-        super(500, 10, name);
+        super(RUN_LIMIT, SWIM_LIMIT, name);
         dogsAmount++;
     }
 
